a4bf8f6b8bd9fbc5fe19e9da1d968e260d2d1652
yasarozankaraman <62656865+yasarozankaraman@users.noreply.github.com>
2022-06-22 15:45:14 +0300

    3

--- Yazilimpark/Repositories/WForecastRepository.cs
+++ Yazilimpark/Repositories/WForecastRepository.cs
@@ -6,12 +6,12 @@ namespace Yazilimpark.Repositories
 {
     public class WForecastRepository : IWForecastRepository
     {
-        public WeatherResponse GetForecast(string city)
+      public WeatherResponse GetForecast(string city)
         {
             string APP_ID = Configiration.Values.OPEN_WEATHER_APP_ID;
             var client = new RestClient($"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={APP_ID}");
-            var request = new RestRequest(Method.Get);
-            RestResponse response = client.Execute(request);
+        /*  //  var request = new RestRequest(Method.Get);
+         //   RestResponse response = client.Execute(request);
 
             if (response.IsSuccessful)
             {
@@ -25,7 +25,7 @@ namespace Yazilimpark.Repositories
                 return null;
 
             }
-            
+            */
         }
     }
 }
